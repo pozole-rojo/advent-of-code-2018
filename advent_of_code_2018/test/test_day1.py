d53f2841cce2e28756a073425d6e6b0d36a9a48c
Pozole Rojo <jperez.4096@gmail.com>
2018-12-05 12:06:58 -0600

    Passing tests with ignoring input dir

--- advent_of_code_2018/test/test_day1.py
+++ advent_of_code_2018/test/test_day1.py
@@ -1,6 +1,8 @@
 from advent_of_code_2018.day_1.day1 import get_num
+from advent_of_code_2018.day_1.inputs.input_1 import get_input_1
 
 
 def test_get_num():
     assert get_num() == "2"
+    print(get_input_1())
     assert True
